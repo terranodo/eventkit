6556d00cfb68b698eeda54a75ac7666f890e0ce5
lukerees <luke.rees@rgi-corp.com>
2016-06-13 11:09:31 -0400

    More geonode stuff

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -115,10 +115,26 @@ sudo grep -q '   ident' /var/lib/pgsql/9.5/data/pg_hba.conf && sed -i "s/   iden
 sudo grep -q '127.0.0.1' /var/lib/pgsql/9.5/data/pg_hba.conf && sed -i "s/127.0.0.1\/32     /192.168.99.120\/32/g" /var/lib/pgsql/9.5/data/pg_hba.conf
 sudo systemctl restart postgresql-9.5
 grep -q '127.0.0.1' /etc/hosts && sed -i "s/127.0.0.1/192.168.99.120/g" /etc/hosts
-service network restart
+sudo service network restart
 
-sudo yum install python-imaging python-virtualenv python-psycopg2 libxml2-devel libxml2-python libxslt-devel libxslt-python -y 
+# SETUP USER AND DB
 sudo adduser -m geonode
+sudo -u postgres psql -c "CREATE USER geonode WITH PASSWORD 'geonode';"
+sudo -u postgres createdb -O geonode geonode
+sudo -u postgres createdb -O geonode geonode_data
+sudo -u postgres psql -d geonode_data -c 'CREATE EXTENSION postgis;'
+sudo -u postgres psql -d geonode_data -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
+sudo -u postgres psql -d geonode_data -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
+
+#INSTALL GEONODE DEPENDENCIES
+sudo yum install python-imaging python-virtualenv python-psycopg2 libxml2-devel libxml2-python libxslt-devel libxslt-python -y 
+sudo yum install httpd -y
+sudo yum install mod_ssl mod_proxy_html mod_wsgi -y
+sudo pip install decorator
+sudo yum install java-1.7.0-openjdk-devel -y 
+sudo yum install tomcat -y
+
+# GEONODE SETUP
 git clone https://github.com/GeoNode/geonode.git
 sudo mv geonode /home/geonode
 cd /home
@@ -127,23 +143,126 @@ sudo chmod -R 755 geonode
 cd /home/geonode/geonode
 sudo pip install -e .
 cd /home/geonode/geonode/geonode
-cp local_settings.py.sample local_settings.py
-sudo echo "ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '::1']" >> /home/geonode/geonode/geonode/local_settings.py
-sudo echo "PROXY_ALLOWED_HOSTS = ('127.0.0.1', 'localhost', '::1')" >> /home/geonode/geonode/geonode/local_settings.py
-sudo echo "POSTGIS_VERSION = (2, 2, 2)"
-sudo grep -q 'http://localhost:8000/' /home/geonode/geonode/geonode/local_settings.py && sed -i "s/http:\/\/localhost:8000/http:\/\/localhost/g" /home/geonode/geonode/geonode/local_settings.py
-sudo grep -q "'ENGINE': ''" /home/geonode/geonode/geonode/local_settings.py && sed -i "s/'ENGINE': ''/# 'ENGINE': ''/g" /home/geonode/geonode/geonode/local_settings.py
-sudo grep -q "#'ENGINE'" /home/geonode/geonode/geonode/local_settings.py && sed -i "s/#'ENGINE'/'ENGINE'/g" /home/geonode/geonode/geonode/local_settings.py
+sudo cp local_settings.py.sample local_settings.py
+sudo echo "ALLOWED_HOSTS = ['192.168.99.120', 'localhost', '::1']" | sudo tee --append /home/geonode/geonode/geonode/local_settings.py
+sudo echo "PROXY_ALLOWED_HOSTS = ('192.168.99.120', 'localhost', '::1')" | sudo tee --append /home/geonode/geonode/geonode/local_settings.py
+sudo echo "POSTGIS_VERSION = (2, 2, 2)" | sudo tee --append /home/geonode/geonode/geonode/local_settings.py
+sudo grep -q 'http://localhost:8000/' /home/geonode/geonode/geonode/local_settings.py && sudo sed -i "s/http:\/\/localhost:8000/http:\/\/localhost/g" /home/geonode/geonode/geonode/local_settings.py
+sudo grep -q "'ENGINE': ''" /home/geonode/geonode/geonode/local_settings.py && sudo sed -i "s/'ENGINE': ''/# 'ENGINE': ''/g" /home/geonode/geonode/geonode/local_settings.py
+sudo grep -q "#'ENGINE'" /home/geonode/geonode/geonode/local_settings.py && sudo sed -i "s/#'ENGINE'/'ENGINE'/g" /home/geonode/geonode/geonode/local_settings.py
 sudo sed -i "0,/'NAME': 'geonode'/! s/'NAME': 'geonode'/'NAME': 'geonode_data'/g" /home/geonode/geonode/geonode/local_settings.py
-#sudo grep -q "'NAME': 'geonode'" /home/geonode/geonode/geonode/local_settings.py && sed -i "s/'NAME': 'geonode'/'NAME': 'geonode_data'/g" /home/geonode/geonode/geonode/local_settings.py
-sudo grep -q "'LOCATION' : 'http://localhost:8080/geoserver/'" /home/geonode/geonode/geonode/local_settings.py && sed -i "s/'LOCATION' : 'http:\/\/localhost:8080\/geoserver\/'/'LOCATION' : 'http:\/\/localhost\/geoserver\/'/g" /home/geonode/geonode/geonode/local_settings.py
-sudo grep -q "'PUBLIC_LOCATION' : 'http://localhost:8080/geoserver/'" /home/geonode/geonode/geonode/local_settings.py && sed -i "s/'PUBLIC_LOCATION' : 'http:\/\/localhost:8080\/geoserver\/'/'PUBLIC_LOCATION' : 'http:\/\/localhost\/geoserver\/'/g" /home/geonode/geonode/geonode/local_settings.py
+sudo grep -q "'LOCATION' : 'http://localhost:8080/geoserver/'" /home/geonode/geonode/geonode/local_settings.py && sudo sed -i "s/'LOCATION' : 'http:\/\/localhost:8080\/geoserver\/'/'LOCATION' : 'http:\/\/localhost\/geoserver\/'/g" /home/geonode/geonode/geonode/local_settings.py
+sudo grep -q "'PUBLIC_LOCATION' : 'http://localhost:8080/geoserver/'" /home/geonode/geonode/geonode/local_settings.py && sudo sed -i "s/'PUBLIC_LOCATION' : 'http:\/\/localhost:8080\/geoserver\/'/'PUBLIC_LOCATION' : 'http:\/\/localhost\/geoserver\/'/g" /home/geonode/geonode/geonode/local_settings.py
+sudo chown geonode:geonode local_settings.py
+sudo chmod 755 local_settings.py
 cd /home/geonode/geonode
-sudo -u postgres createdb geonode
-sudo -u postgres psql -c "CREATE USER geonode WITH PASSWORD 'geonode';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE geonode to geonode"
-#sudo -u postgres psql -c "CREATE DATABASE geonode_data;"
+sudo chmod 777 /usr/lib/python2.7/site-packages/account
+sudo -u geonode python /home/geonode/geonode/manage.py makemigrations sites
+sudo -u geonode python /home/geonode/geonode/manage.py migrate sites
+sudo -u geonode python /home/geonode/geonode/manage.py makemigrations account
+sudo -u geonode python /home/geonode/geonode/manage.py migrate account
+# sudo -u geonode python /home/geonode/geonode/manage.py makemigrations
+# sudo -u geonode python /home/geonode/geonode/manage.py migrate
 sudo -u geonode python /home/geonode/geonode/manage.py syncdb --noinput
+sudo -u geonode python /home/geonode/geonode/manage.py createsuperuser --username admin --email admin@geonode.com --noinput
+sudo -u geonode python /home/geonode/geonode/manage.py collectstatic --noinput
+sudo mkdir /home/geonode/geonode/geonode/uploaded/
+sudo chmod +X /home/geonode/
+sudo chown -R geonode /home/geonode/geonode/
+sudo chown apache:apache /home/geonode/geonode/geonode/static/
+sudo chown apache:apache /home/geonode/geonode/geonode/uploaded/
+sudo chown apache:apache /home/geonode/geonode/geonode/static_root/
+
+# TOMCAT SETUP
+sudo mkdir /var/lib/tomcats/base
+sudo cp -a /usr/share/tomcat/* /var/lib/tomcats/base/
+sudo mkdir /var/lib/tomcats/geoserver
+sudo cp -a /usr/share/tomcat/* /var/lib/tomcats/geoserver/
+sudo cp /usr/lib/systemd/system/tomcat.service /usr/lib/systemd/system/tomcat\@geoserver.service
+sudo grep -q "EnvironmentFile=-/etc/sysconfig/tomcat" /usr/lib/systemd/system/tomcat\@geoserver.service && sudo sed -i "s/EnvironmentFile=-\/etc\/sysconfig\/tomcat/EnvironmentFile=-\/etc\/sysconfig\/tomcat@geoserver/g" /usr/lib/systemd/system/tomcat\@geoserver.service
+sudo cp /etc/sysconfig/tomcat /etc/sysconfig/tomcat\@geoserver
+sudo grep -q '#CATALINA_BASE' /etc/sysconfig/tomcat\@geoserver && sudo sed -i 's/#CATALINA_BASE/CATALINA_BASE/g' /etc/sysconfig/tomcat\@geoserver
+sudo grep -q 'CATALINA_BASE="\/usr\/share\/tomcat"' /etc/sysconfig/tomcat\@geoserver && sudo sed -i 's/CATALINA_BASE="\/usr\/share\/tomcat"/CATALINA_BASE="\/var\/lib\/tomcats\/geoserver"/g' /etc/sysconfig/tomcat\@geoserver
+cd /var/lib/tomcats/geoserver/webapps/
+sudo wget http://build.geonode.org/geoserver/latest/geoserver.war
+
+sudo chown -R tomcat:tomcat /var/lib/tomcats*
+sudo systemctl start tomcat@geoserver
+sudo systemctl enable tomcat@geoserver
+
+# APACHE SETUP
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --zone=public --add-service=http --permanent
+sudo firewall-cmd --reload
+sudo setsebool -P httpd_can_network_connect_db 1
+sudo su -c "echo '
+WSGIDaemonProcess geonode python-path=/home/geonode/geonode:/home/geonode/.venvs/geonode/lib/python2.7/site-packages user=apache threads=15 processes=2
+
+<VirtualHost *:80>
+    ServerName http://localhost
+    ServerAdmin webmaster@localhost
+    DocumentRoot /home/geonode/geonode/geonode
+
+    ErrorLog /var/log/httpd/error.log
+    LogLevel warn
+    CustomLog /var/log/httpd/access.log combined
+
+    WSGIProcessGroup geonode
+    WSGIPassAuthorization On
+    WSGIScriptAlias / /home/geonode/geonode/geonode/wsgi.py
+
+    Alias /static/ /home/geonode/geonode/geonode/static_root/
+    Alias /uploaded/ /home/geonode/geonode/geonode/uploaded/
+
+    <Directory "/home/geonode/geonode/geonode/">
+         <Files wsgi.py>
+             Order deny,allow
+             Allow from all
+             Require all granted
+         </Files>
+
+        Order allow,deny
+        Options Indexes FollowSymLinks
+        Allow from all
+        IndexOptions FancyIndexing
+    </Directory>
+
+    <Directory "/home/geonode/geonode/geonode/static_root/">
+        Order allow,deny
+        Options Indexes FollowSymLinks
+        Allow from all
+        Require all granted
+        IndexOptions FancyIndexing
+    </Directory>
+
+    <Directory "/home/geonode/geonode/geonode/uploaded/thumbs/">
+        Order allow,deny
+        Options Indexes FollowSymLinks
+        Allow from all
+        Require all granted
+        IndexOptions FancyIndexing
+    </Directory>
+
+    <Proxy *>
+        Order allow,deny
+        Allow from all
+    </Proxy>
+
+    ProxyPreserveHost On
+    ProxyPass /geoserver http://192.168.99.120:8080/geoserver
+    ProxyPassReverse /geoserver http://192.168.99.120:8080/geoserver
+
+</VirtualHost>' >> /etc/httpd/conf.d/geonode.conf"
+sudo mkdir -p /home/geonode/geonode/geonode/uploaded/thumbs
+sudo mkdir -p /home/geonode/geonode/geonode/uploaded/layers
+sudo chown -R geonode /home/geonode/geonode/
+sudo chown geonode:apache /home/geonode/geonode/geonode/static/
+sudo chown geonode:apache /home/geonode/geonode/geonode/uploaded/
+sudo chmod -Rf 777 /home/geonode/geonode/geonode/uploaded/thumbs
+sudo chmod -Rf 777 /home/geonode/geonode/geonode/uploaded/layers
+sudo chown apache:apache /home/geonode/geonode/geonode/static_root/
+
 
 #cd /var/lib/osm-extract
 #sudo -u postgres make clean all NAME=guinea_bissau URL=http://download.geofabrik.de/africa/guinea-bissau-latest.osm.pbf
